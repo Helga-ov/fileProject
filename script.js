1c5915a2edba03ed72caf607bc5f912148e74c87
Helga-ov <115106871+Helga-ov@users.noreply.github.com>
2022-12-02 16:26:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,50 @@ function dCity(event) {
   axios.get(apiUrl).then(displayTemperature);
 }
 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
 let citySearch1 = document.querySelector("#searchForm");
 citySearch1.addEventListener("submit", dCity);
 
@@ -60,4 +104,6 @@ function displayTemperature(response) {
   );
   let description1 = document.querySelector("#description");
   description1.innerHTML = response.data.weather[0].description;
+  
+  getForecast(response.data.coord);
 }
